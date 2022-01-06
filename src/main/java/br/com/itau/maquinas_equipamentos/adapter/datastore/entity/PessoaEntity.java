d79cf139949ed480db2e64c00f7bbed31cee25e0
Ícaro Garcez <allegarcezz@gmail.com>
2022-01-06 00:31:49 -0300

    Adicionando atributos e colunas na entidade de Pessoa

--- src/main/java/br/com/itau/maquinas_equipamentos/adapter/datastore/entity/PessoaEntity.java
+++ src/main/java/br/com/itau/maquinas_equipamentos/adapter/datastore/entity/PessoaEntity.java
@@ -1,5 +1,55 @@
 package br.com.itau.maquinas_equipamentos.adapter.datastore.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TBGG081_CTTO_PESS")
 public class PessoaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "COD_IDT_CTTO_PESS", nullable = false)
+	private String idContratoPessoa;
+
+	@Column(name = "COD_TIPO_RLMT_GARA_PESS", nullable = false)
+	private int idTipoRelacionamentoGarantiaPessoa;
+
+	@Column(name = "COD_TIPO_PESS_CTTO_GRUP")
+	private char idTipoPessoaContratoGrupo;
+
+	@Column(name = "NUM_CPF_CNPJ_CTTO_GRUP")
+	private double cpfOuCnpjContratoGrupo;
+
+	@Column(name = "COD_TIPO_PESS_CTT")
+	private char idTipoPessoaContrato;
+
+	@Column(name = "NUM_CPF_CNPJ_CTTO_PESS")
+	private double cpfOuCnpjContratoPessoa;
+
+	@Column(name = "NOM_CTTO_PESS")
+	private String nome;
+
+	@Column(name = "NOM_EMAI_CTTO_PESS")
+	private String email;
+
+	@Column(name = "COD_DDD_TEL_CTTO_PESS")
+	private int dddTelefone;
+
+	@Column(name = "NUM_TEL_CTTO_PESS")
+	private double telefone;
 
 }
